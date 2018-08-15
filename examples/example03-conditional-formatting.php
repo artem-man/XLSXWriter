c393d2bfd0e1bcbf6332c06df11a797fd8838e9c
artem-man <artyom.mirgorodsky@gmail.com>
2018-08-15 13:54:30 +0300

    addded example for condition formatting

--- examples/example03-conditional-formatting.php
+++ examples/example03-conditional-formatting.php
@@ -22,14 +22,14 @@ for($i=0; $i<1000; $i++)
 	$s2 = substr($s, rand()%8000, rand()%5+5);
 	$s3 = substr($s, rand()%12000, rand()%5+5);
 	$s4 = substr($s, rand()%16000, rand()%5+5);
-    $sheet->writeRow(array($n1, $n2, $n3, $n4, $s1, $s2, $s3, $s4), array([],['number_format' => '$#,##0.00'],[],['number_format' => '#0.00%'],['font'=>'Arial','font-size'=>10,'font-style'=>'bold', 'fill'=>'#eee', 'halign'=>'center', 'border'=>'left,right,top,bottom'],[],[],[]) );
+    $sheet->writeRow(array($n1, $n2, $n3, $n4, $s1, $s2, $s3, $s4), array(['number_format' => 'GENERAL'],['number_format' => '$#,##0.00'],[],['number_format' => '#0.00%'],['font'=>'Arial','font-size'=>10,'font-style'=>'bold', 'fill'=>'#eee', 'halign'=>'center', 'border'=>'left,right,top,bottom'],[],[],[]) );
 }
 
 $sheet->setConditionalStyle('E1:H1001','$A1<=0',array('color' => '#f00'));
 $sheet->setConditionalStyle('E1:H1001','$A1>=1000',array('fill' => '#0f0'));
 $sheet->setConditionalStyle('E1:H1001','AND($A1>0, $A1<1000)',array('fill' => '#00f'));
 $sheet->setConditionalStyle('A1:D1001','$B1>1000',array('fill' => '#ee6440'));
-$sheet->setConditionalStyle('C1:C1001','$C1*10<5',array('border'=>'top,bottom', 'align' => 'center'));
+$sheet->setConditionalStyle('C1:C1001','$C1*10<5',array('border'=>'top,bottom', 'align' => 'center', 'number_format' => '#0.0%'));
 
 
 $xlsx->writeToFile(str_replace('.php', '.xlsx',$_SERVER['argv'][0]));
